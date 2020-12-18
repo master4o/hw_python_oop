65be515e19e2e346c4e7066b8c61aad291b71ffc
Alexandr <master4o@bk.ru>
2020-12-18 16:01:48 +0300

    first itteration

--- homework.py
+++ homework.py
@@ -44,8 +44,8 @@ class Calculator:
 
 
 class CashCalculator(Calculator):
-    USD_RATE = 72.81
-    EURO_RATE = 89.11
+    USD_RATE = 60.00
+    EURO_RATE = 70.00
     rates = {'usd': (USD_RATE, 'USD'),
              'eur': (EURO_RATE, 'Euro'),
              'rub': (1, 'руб')
@@ -76,3 +76,16 @@ class CaloriesCalculator(Calculator):
                     f'но с общей калорийностью не более {today_calories_balance} кКал')
         else:
             return f'Хватит есть!'
+
+
+cash_calculator = CashCalculator(1000)
+
+# дата в параметрах не указана,
+# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment="кофе"))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
+
+print(cash_calculator.get_today_cash_remained("eur"))
